e32222a1bcdfa8223bc99b2ae478ebc8811d3d15
Sumit Risal <risal_305@hotmail.com>
2023-09-17 14:32:30 +0100

    Branch2 Commit

--- src/SubTest1.java
+++ src/SubTest1.java
@@ -3,5 +3,6 @@ import java.sql.SQLOutput;
 public class SubTest1 {
     public static void main(String[] args) {
         System.out.println("hello Branch");
+        System.out.println("hi");
     }
 }
